e8865c5e84db02603ea04801088d3752e1ae73be
CVRobo <ktaneja0910@gmail.com>
2026-09-09 16:08:02 -0400

    update 2- improved function safety and added load function

--- ssm_interpreter.py
+++ ssm_interpreter.py
@@ -1,27 +1,50 @@
+import sys
 stack=[]
 store={}
+def error(msg):
+    print(f"Error: {msg}", file=sys.stderr)
+    sys.exit(1)
+def safe_pop():
+    if not stack:
+        error("pop from empty stack")
+    return stack.pop()
 def ildc(a):
     try:
         int(a)
-        stack.append(a)
+        stack.append(int(a))
     except (ValueError, TypeError):
         return
 def iadd(a,b):
-    return a+b
+    b = safe_pop()
+    a = safe_pop()
+    stack.append(a + b)
 def isub(a,b):
-    return a-b
+    b = safe_pop()
+    a = safe_pop()
+    stack.append(a - b)
 def imul(a,b):
-    return a*b
+    b = safe_pop()
+    a = safe_pop()
+    stack.append(a * b)
 def idiv(a,b):
-    return a/b
+    b = safe_pop()
+    a = safe_pop()
+    if b == 0:
+        error("division by zero")
+    stack.append(a// b)
 def imod(a,b):
-    return a%b
+    b = safe_pop()
+    a = safe_pop()
+    stack.append(a % b)
 #for pop we can just use python's pop
 def dup():
     stack.append(stack[0])
 #TODO: JZ label, jnz label, jmp label
 def store():
-    i=stack.pop()
-    a=stack.pop()
+    b = safe_pop()
+    a = safe_pop()
     store[a]=i
-    
+def strip_comments(text):
+    lines = text.split('\n')
+    cleaned = [line.split('#', 1)[0] for line in lines]
+    return '\n'.join(cleaned)
