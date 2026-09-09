c4aeb810088637d242bc5d5d5e5da3ba461e02f4
CVRobo <ktaneja0910@gmail.com>
2026-09-09 15:53:28 -0400

    update 1- basic functions

---
+++ ssm_interpreter.py
@@ -0,0 +1,27 @@
+stack=[]
+store={}
+def ildc(a):
+    try:
+        int(a)
+        stack.append(a)
+    except (ValueError, TypeError):
+        return
+def iadd(a,b):
+    return a+b
+def isub(a,b):
+    return a-b
+def imul(a,b):
+    return a*b
+def idiv(a,b):
+    return a/b
+def imod(a,b):
+    return a%b
+#for pop we can just use python's pop
+def dup():
+    stack.append(stack[0])
+#TODO: JZ label, jnz label, jmp label
+def store():
+    i=stack.pop()
+    a=stack.pop()
+    store[a]=i
+    
