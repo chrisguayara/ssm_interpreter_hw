aceba0c5365b14cffe2d7e96ee07c81a572af3d3
chrisguayara <chrisg.deu@gmail.com>
2026-09-09 16:28:13 -0400

    split empty lines add

--- ssm_interpreter.py
+++ ssm_interpreter.py
@@ -44,6 +44,12 @@ def load():
 def dup():
     stack.append(stack[0])
 #TODO: JZ label, jnz label, jmp label
+
+def jz_label(lbl):
+    lbl = safe_pop()
+    if lbl == 0:
+        pass #TODO
+
 def store():
     b = safe_pop()
     a = safe_pop()
@@ -52,12 +58,25 @@ def strip_comments(text):
     lines = text.split('\n')
     cleaned = [line.split('#', 1)[0] for line in lines]
     return '\n'.join(cleaned)
-
     
+def strip_empties(text):
+    text = strip_comments(text)
+
+    res = [sub.split() for sub in text.split('\n')]
+    return [line for line in res if line]
 
 
 def main():
-    filename = argv
-    txt = open(filename)
+    filename = sys.argv[1]
 
+    with open(filename) as f:
+       txt = f.read() 
+    strip_empties(txt)
     print(txt)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
